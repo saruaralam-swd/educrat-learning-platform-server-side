e052a1aaea16402a065f05387dbd17fd97af9a22
saruaralam-swd <saruaralam2001@gmail.com>
2022-10-26 01:37:47 +0600

    courese details data add and route create

--- index.js
+++ index.js
@@ -6,14 +6,21 @@ app.use(cors());
 
 const port = process.env.PORT || 5000;
 
-const allCourses = require('./data/courses.json');
+const courses = require('./data/courses.json');
+const courseDetails = require('./data/course.json');
 
 app.get('/', (req, res) => {
   res.send('server is running');
 });
 
 app.get('/courses', (req, res) => {
-  res.send(allCourses);
+  res.send(courses);
+});
+
+app.get('/courses/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const details  = courseDetails.find(c => c.categoryId === id);
+  res.send(details);  
 })
 
 app.listen(port, () => {
