2129cd464494ea8cf76989a7b4e907e43e65dc21
saruaralam-swd <saruaralam2001@gmail.com>
2022-10-25 23:36:54 +0600

    courese data add

--- index.js
+++ index.js
@@ -6,9 +6,15 @@ app.use(cors());
 
 const port = process.env.PORT || 5000;
 
+const allCourses = require('./data/courses.json');
+
 app.get('/', (req, res) => {
   res.send('server is running');
 });
+
+app.get('/courses', (req, res) => {
+  res.send(allCourses);
+})
 
 app.listen(port, () => {
   console.log('server is running on port', port);
