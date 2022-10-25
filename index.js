d38c31ac039a088b580f289cdbb8299fb7b96d33
saruaralam-swd <saruaralam2001@gmail.com>
2022-10-25 18:12:49 +0600

    now my problem sovle

--- index.js
+++ index.js
@@ -8,10 +8,8 @@ const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
   res.send('server is running');
-})
+});
 
 app.listen(port, () => {
   console.log('server is running on port', port);
-})
-
-// for text and check gitignore 
+});
